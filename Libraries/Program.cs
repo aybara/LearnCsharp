1efac11311d9b95780cfc0e5549e28e876779922
LEANDRO COELHO <leandrogcf@gmail.com>
2019-07-16 17:47:34 -0300

    a

--- Libraries/Program.cs
+++ Libraries/Program.cs
@@ -8,6 +8,7 @@ using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Configuration.ConfigurationBuilders;
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
@@ -31,94 +32,9 @@ namespace Libraries
 
             //string secret = azureKeyVault.GetSecretAsync("HelloWorld").GetAwaiter().GetResult();
 
-            AzureQueueProducerConsumer<string> azureQueue = new AzureQueueProducerConsumer<string>("DefaultEndpointsProtocol=https;AccountName=storageaccountqueues;AccountKey=XpJZXvRJhJRmEz4HkyxMeTZJ31v1qWHGztDiBAJsqgobjhNouaYkt1+DGd/TDobtsqRK/xJ+c6fPimgXRZh/7g==;EndpointSuffix=core.windows.net", "facebookqueue");
-            
-            var messages = azureQueue.queue.PeekMessages(32);
-            List<JsonData> jsonDatas = new List<JsonData>();
-            foreach (var message in messages)
-            {
-                jsonDatas.Add(JsonConvert.DeserializeObject<JsonData>(message.AsString));
-            }
-            jsonDatas.Where(data => data.Entry[0].Changed_Fields.Contains(""));
+            var teste = ConfigurationManager.AppSettings.Get("secret1");
+            var teste2 = Environment.GetEnvironmentVariables();
         }
     }
-    public class JsonData
-    {
-        [JsonProperty("entry")]
-        public List<Entry> Entry { get; set; }
-
-        [JsonProperty("object")]
-        public string Object { get; set; }
-    }
-    public class Entry
-    {
-        [JsonProperty("changed_fields")]
-        public List<string> Changed_Fields { get; set; }
-
-        [JsonProperty("id")]
-        public string Id { get; set; }
-
-        [JsonProperty("time")]
-        public int Time { get; set; }
-    }
-
-    public class Value
-    {
-        [JsonProperty("ad_id")]
-        public string AdId { get; set; }
-
-        [JsonProperty("form_id")]
-        public string FormId { get; set; }
-
-        [JsonProperty("leadgen_id")]
-        public string LeadGenId { get; set; }
-
-        [JsonProperty("created_time")]
-        public int CreatedTime { get; set; }
-
-        [JsonProperty("page_id")]
-        public string PageId { get; set; }
-
-        [JsonProperty("adgroup_id")]
-        public string AdGroupId { get; set; }
-    }
-
-    public class LeadData
-    {
-        [JsonProperty("created_time")]
-        public string CreatedTime { get; set; }
-
-        [JsonProperty("id")]
-        public string Id { get; set; }
-
-        [JsonProperty("field_data")]
-        public List<FieldData> FieldData { get; set; }
-    }
-
-    public class FieldData
-    {
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
-        [JsonProperty("values")]
-        public List<string> Values { get; set; }
-    }
-
-    public class LeadFormData
-    {
-        [JsonProperty("id")]
-        public string Id { get; set; }
-
-        [JsonProperty("leadgen_export_csv_url")]
-        public string CsvExportUrl { get; set; }
-
-        [JsonProperty("locale")]
-        public string Locale { get; set; }
-
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
-        [JsonProperty("status")]
-        public string Status { get; set; }
-    }
+    
 }
