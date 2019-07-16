3ddbb4ce506ef307d0da6f15b14ba0f4cdde4690
Leandro Coelho <leandrogcf@gmail.com>
2019-07-16 10:40:43 -0300

    SecureKeys class

--- Libraries/Program.cs
+++ Libraries/Program.cs
@@ -17,6 +17,7 @@ using Libraries.Cache;
 using Libraries.Queues;
 using Libraries.Math;
 using Newtonsoft.Json;
+using MyClassLibrary;
 
 namespace Libraries
 {
@@ -30,95 +31,9 @@ namespace Libraries
             //    ConfigurationManager.AppSettings.Get("AzureKeyVaultUrl"));
 
             //string secret = azureKeyVault.GetSecretAsync("HelloWorld").GetAwaiter().GetResult();
-
-            AzureQueueProducerConsumer<string> azureQueue = new AzureQueueProducerConsumer<string>("DefaultEndpointsProtocol=https;AccountName=storageaccountqueues;AccountKey=XpJZXvRJhJRmEz4HkyxMeTZJ31v1qWHGztDiBAJsqgobjhNouaYkt1+DGd/TDobtsqRK/xJ+c6fPimgXRZh/7g==;EndpointSuffix=core.windows.net", "facebookqueue");
             
-            var messages = azureQueue.queue.PeekMessages(32);
-            List<JsonData> jsonDatas = new List<JsonData>();
-            foreach (var message in messages)
-            {
-                jsonDatas.Add(JsonConvert.DeserializeObject<JsonData>(message.AsString));
-            }
-            jsonDatas.Where(data => data.Entry[0].Changed_Fields.Contains(""));
+            SecureKeys.SetSecret("secret3", "Leandro");
         }
     }
-    public class JsonData
-    {
-        [JsonProperty("entry")]
-        public List<Entry> Entry { get; set; }
-
-        [JsonProperty("object")]
-        public string Object { get; set; }
-    }
-    public class Entry
-    {
-        [JsonProperty("changed_fields")]
-        public List<string> Changed_Fields { get; set; }
-
-        [JsonProperty("id")]
-        public string Id { get; set; }
-
-        [JsonProperty("time")]
-        public int Time { get; set; }
-    }
-
-    public class Value
-    {
-        [JsonProperty("ad_id")]
-        public string AdId { get; set; }
-
-        [JsonProperty("form_id")]
-        public string FormId { get; set; }
-
-        [JsonProperty("leadgen_id")]
-        public string LeadGenId { get; set; }
-
-        [JsonProperty("created_time")]
-        public int CreatedTime { get; set; }
-
-        [JsonProperty("page_id")]
-        public string PageId { get; set; }
-
-        [JsonProperty("adgroup_id")]
-        public string AdGroupId { get; set; }
-    }
-
-    public class LeadData
-    {
-        [JsonProperty("created_time")]
-        public string CreatedTime { get; set; }
-
-        [JsonProperty("id")]
-        public string Id { get; set; }
-
-        [JsonProperty("field_data")]
-        public List<FieldData> FieldData { get; set; }
-    }
-
-    public class FieldData
-    {
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
-        [JsonProperty("values")]
-        public List<string> Values { get; set; }
-    }
-
-    public class LeadFormData
-    {
-        [JsonProperty("id")]
-        public string Id { get; set; }
-
-        [JsonProperty("leadgen_export_csv_url")]
-        public string CsvExportUrl { get; set; }
-
-        [JsonProperty("locale")]
-        public string Locale { get; set; }
-
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
-        [JsonProperty("status")]
-        public string Status { get; set; }
-    }
+    
 }
---
+++ MyClassLibrary/SecureKeys.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MyClassLibrary
+{
+    public static class SecureKeys
+    {
+        private static string FilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SecureKeys/secrets.xml");
+
+        public static string GetSecret(string secretName)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(FilePath);
+
+            var secrets = document.GetElementsByTagName("secret").GetEnumerator();
+            while (secrets.MoveNext())
+                if(((XmlElement)secrets.Current).Attributes["name"].Value == secretName)
+                    return ((XmlElement)secrets.Current).Attributes["value"].Value;
+
+            return string.Empty;
+        }
+
+        public static void SetSecret(string secretName, string secretValue)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(FilePath);
+
+            var secrets = document.GetElementsByTagName("secret").GetEnumerator();
+            while (secrets.MoveNext())
+                if (((XmlElement)secrets.Current).Attributes["name"].Value == secretName)
+                    return;
+
+            XmlNode secretsNode = document.FirstChild.FirstChild;
+            XmlElement newElement = document.CreateElement("secret");
+            XmlAttribute name = document.CreateAttribute("name");
+            name.Value = secretName;
+            XmlAttribute value = document.CreateAttribute("value");
+            value.Value = secretValue;
+            newElement.Attributes.Append(name);
+            newElement.Attributes.Append(value);
+            secretsNode.AppendChild(newElement);
+            document.Save(FilePath);
+        }
+    }
+}
