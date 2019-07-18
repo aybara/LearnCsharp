d99c986e26e5902fc46a92cd25213f98289fee2b
LEANDRO COELHO <leandrogcf@gmail.com>
2019-07-17 21:36:47 -0300

    a

---
+++ MyClassLibrary/AzureServices/AzureQueueHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+namespace MyClassLibrary.AzureServices
+{
+    public class AzureQueueHandler : QueueClient
+    {
+        public AzureQueueHandler(string queueName, string connectionStorageStringName = "MyConnectionStorageString") : base(SecureSecrets.GetSecret(connectionStorageStringName), queueName)
+        {
+            this.CreateAsync().GetAwaiter();
+        }
+    }
+}
---
+++ MyClassLibrary/AzureServices/AzureServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Storage;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+namespace MyClassLibrary.AzureServices
+{
+    public class AzureServices
+    {
+        public QueueClient queue;
+        public AzureServices() { }
+    }
+}
